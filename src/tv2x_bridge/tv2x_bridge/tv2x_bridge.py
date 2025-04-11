63e480ce2000117141ac29d8d5c0833331e450cf
Edoardo Torrini <edoardo.torrini@gmail.com>
2025-04-11 12:02:39 +0200

    update tv2x_bridge node

--- src/tv2x_bridge/tv2x_bridge/tv2x_bridge.py
+++ src/tv2x_bridge/tv2x_bridge/tv2x_bridge.py
@@ -4,6 +4,8 @@ from rclpy.node import Node
 from scapy.all import sniff, Raw
 import asn1tools
 
+from threading import Thread
+
 import os
 
 class V2Xbridge(Node):
@@ -12,21 +14,31 @@ class V2Xbridge(Node):
         super().__init__("tv2x_bridge")
 
         self.load_parameters()
-        self.cam = asn1tools.compile_files(self.asn_file, "uper")
-        sniff(iface=self.v2x_interface, prn=self.cam_callback, count=10)
+        self.cam = asn1tools.compile_files(self.asn_file_cam, "uper")
+        self.denm = asn1tools.compile_files(self.asn_file_denm, "uper")
+
+        self.tshark = Thread(self.start_listener_v2x, daemon=True)
+        self.tshark.start()
+        
 
     def load_parameters(self):
         self.declare_parameter("general.v2x_interface", "")
         self.v2x_interface = self.get_parameter("general.v2x_interface").value
 
-        self.declare_parameter("general.asn_file", "")
-        self.asn_file = self.get_parameter("general.asn_file").value
+        self.declare_parameter("general.asn_file_cam", "")
+        self.asn_file_cam = self.get_parameter("general.asn_file_cam").value
+
+        self.declare_parameter("general.asn_file_denm", "")
+        self.asn_file_denm = self.get_parameter("general.asn_file_denm").value
 
         self.declare_parameter("topic.cam", "")
         self.topic_cam = self.get_parameter("topic.cam").value
         
         self.declare_parameter("topic.denm", "")
         self.topic_denm = self.get_parameter("topic.denm").value
+
+    def start_listener_v2x(self):
+        sniff(iface=self.v2x_interface, prn=self.cam_callback, count=10)
 
     def cam_callback(self, packet):
         if packet.haslayer(Raw):
